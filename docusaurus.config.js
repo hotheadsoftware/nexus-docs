c3fe339fc2b028136862953af967c993a476d586
Zach Mowrey <zbmowrey@gmail.com>
2024-01-11 11:37:52 -0500

    Fix paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://hotheadsoftware.github.io/nexus-docs',
+  url: 'https://hotheadsoftware.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/nexus-docs/',
